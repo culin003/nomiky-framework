74be203561610404e3ae7083a0b8d572ab7c2602
nomiky <culin003!@126.com>
2023-12-21 23:18:54 +0800

    补充查询操作

--- src/main/java/org/nomiky/nomikyframework/bean/DaoExecutorBeanProcessor.java
+++ src/main/java/org/nomiky/nomikyframework/bean/DaoExecutorBeanProcessor.java
@@ -5,15 +5,26 @@
  */
 package org.nomiky.nomikyframework.bean;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.lang.Pair;
+import cn.hutool.core.map.MapUtil;
+import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.StrUtil;
+import org.nomiky.nomikyframework.constant.DaoConstants;
 import org.nomiky.nomikyframework.entity.Page;
 import org.nomiky.nomikyframework.entity.TableDefinition;
 import org.nomiky.nomikyframework.exception.ExecutorException;
 import org.nomiky.nomikyframework.executor.DaoExecutor;
 import org.nomiky.nomikyframework.util.Checker;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.DataClassRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 
 import java.io.Serializable;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -119,23 +130,120 @@ public class DaoExecutorBeanProcessor {
             }
 
             public List<Map<String, Object>> select(Map<String, Object> valuesMap) {
-                return null;
+                Pair<String, Object[]> parseResult = parseSelectSql(valuesMap, false, false);
+                return jdbcTemplate.query(parseResult.getKey(), (rs, rowNum) -> {
+                    Map<String, Object> resultMap = new HashMap<>();
+                    for (String column : tableDefinition.getColumns().keySet()) {
+                        resultMap.put(column, rs.getObject(column));
+                    }
+                    return resultMap;
+                }, parseResult.getValue());
             }
 
             public Map<String, Object> selectOne(Map<String, Object> valuesMap) {
-                return null;
+                Pair<String, Object[]> parseResult = parseSelectSql(valuesMap, false, true);
+                List<Map<String, Object>> result = jdbcTemplate.query(parseResult.getKey(), (rs, rowNum) -> {
+                    Map<String, Object> resultMap = new HashMap<>();
+                    for (String column : tableDefinition.getColumns().keySet()) {
+                        resultMap.put(column, rs.getObject(column));
+                    }
+                    return resultMap;
+                }, parseResult.getValue());
+                return CollUtil.isEmpty(result) ? new HashMap<>(0) : result.get(0);
             }
 
             public Boolean exist(Map<String, Object> valuesMap) {
-                return null;
+                Pair<String, Object[]> parseResult = parseSelectSql(valuesMap, true, true);
+                Long count = jdbcTemplate.query(parseResult.getKey(), rs -> {
+                    return rs.getLong(1);
+                }, parseResult.getValue());
+                return null == count ? Boolean.FALSE : count > 0;
             }
 
             public Long count(Map<String, Object> valuesMap) {
-                return null;
+                Pair<String, Object[]> parseResult = parseSelectSql(valuesMap, true, false);
+                Long count = jdbcTemplate.query(parseResult.getKey(), rs -> {
+                    return rs.getLong(1);
+                }, parseResult.getValue());
+                return null == count ? 0 : count;
             }
 
             public Page selectPage(Map<String, Object> valuesMap) {
-                return null;
+                Page page = new Page();
+                if (!valuesMap.containsKey(DaoConstants.PAGING_SIZE)
+                        || !valuesMap.containsKey(DaoConstants.PAGING_CURRENT)) {
+                    return new Page();
+                }
+
+                Long count = count(valuesMap);
+                page.setCurrent((Long) valuesMap.get(DaoConstants.PAGING_CURRENT));
+                page.setSize((Long) valuesMap.get(DaoConstants.PAGING_SIZE));
+                if (count <= 0) {
+                    page.setTotal(0);
+                    page.setRecords(new ArrayList<>(0));
+                    return page;
+                }
+
+                page.setTotal(count);
+                Pair<String, Object[]> parseResult = parseSelectPageSql(valuesMap);
+                List<Map<String, Object>> result = jdbcTemplate.query(parseResult.getKey(), (rs, rowNum) -> {
+                    Map<String, Object> resultMap = new HashMap<>();
+                    for (String column : tableDefinition.getColumns().keySet()) {
+                        resultMap.put(column, rs.getObject(column));
+                    }
+                    return resultMap;
+                }, parseResult.getValue());
+
+                page.setRecords(result);
+                return page;
+            }
+
+            private Pair<String, Object[]> parseSelectPageSql(Map<String, Object> valuesMap){
+                Pair<String, Object[]>  parseResult = parseSelectSql(valuesMap, false, false);
+                String sql = parseResult.getKey();
+                Object[] params = parseResult.getValue();
+                sql += " LIMIT ?, ?";
+                int index = 0;
+                if (ArrayUtil.isEmpty(params)) {
+                    params = new Object[2];
+                }else{
+                    index = params.length - 1;
+                    params = ArrayUtil.resize(params, params.length + 2);
+                }
+
+                Integer current = (Integer) valuesMap.get(DaoConstants.PAGING_CURRENT);
+                Integer pageSize = (Integer) valuesMap.get(DaoConstants.PAGING_SIZE);
+                params[++index] = (current - 1) * pageSize;
+                params[++index] = pageSize;
+                return new Pair<>(sql, params);
+            }
+
+            private Pair<String, Object[]> parseSelectSql(Map<String, Object> valuesMap, boolean isCount, boolean useLimitOne) {
+                StringBuilder sqlBuilder = new StringBuilder();
+                Set<String> columns = tableDefinition.getColumns().keySet();
+                sqlBuilder.append("SELECT ")
+                        .append(isCount ? "COUNT(*)" : StrUtil.join(",", columns))
+                        .append(" FROM ")
+                        .append(tableDefinition.getName());
+                Object[] params = null;
+                if (MapUtil.isNotEmpty(valuesMap)) {
+                    int index = 0;
+                    params = new Object[valuesMap.size()];
+                    sqlBuilder.append(" WHERE ");
+                    for (Map.Entry<String, Object> entry : valuesMap.entrySet()) {
+                        sqlBuilder.append((index == 0) ? StrUtil.EMPTY : " AND ")
+                                .append(entry.getKey())
+                                .append(" = ?");
+                        params[index] = entry.getValue();
+                        index++;
+                    }
+                }
+
+                if (useLimitOne) {
+                    sqlBuilder.append(" LIMIT 1");
+                }
+
+                return new Pair<>(sqlBuilder.toString(), params);
             }
         };
     }
---
+++ src/main/java/org/nomiky/nomikyframework/constant/DaoConstants.java
@@ -0,0 +1,24 @@
+package org.nomiky.nomikyframework.constant;
+
+public interface DaoConstants {
+
+    /**
+     * 分页参数：当前页
+     */
+    String PAGING_CURRENT = "pageCurrent";
+
+    /**
+     * 分页参数：每页数据量
+     */
+    String PAGING_SIZE = "pageSize";
+
+    /**
+     * 分页参数：查询到的数据总数
+     */
+    String PAGING_TOTAL = "pageTotal";
+
+    /**
+     * 分页参数：数据
+     */
+    String PAGING_RECORDS = "pageRecords";
+}
